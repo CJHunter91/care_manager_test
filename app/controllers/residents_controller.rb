4e0e35ddcd0fbcfd3941151c73f3d607ed9e6b0e
Chris Hunter <orkem2@gmail.com>
2017-10-03 15:26:34 +0100

    adds resident controller and routes

---
+++ app/controllers/residents_controller.rb
@@ -0,0 +1,22 @@
+class ResidentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'residents/index'
+
+  get 'residents/show'
+
+  get 'residents/create'
+
+  get 'residents/new'
+
+  get 'residents/destroy'
+
+  get 'residents/edit'
+
+  get 'residents/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
