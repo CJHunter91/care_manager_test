d4c7299ceb81b66fb98ed5599bd9225da064a3c0
Chris Hunter <orkem2@gmail.com>
2017-10-04 14:39:42 +0100

    adds all tests for routes

--- test/controllers/residents_controller_test.rb
+++ test/controllers/residents_controller_test.rb
@@ -10,8 +10,24 @@ class ResidentsControllerTest < ActionDispatch::IntegrationTest
     assert_routing({ method: 'get', path: '/residents/1' }, { controller: "residents", action: "show", id: "1" })
   end
 
+  test "should create resident" do 
+    assert_routing({ method: 'post', path: '/residents' }, { controller: "residents", action: "create"})
+  end
+
+  test "should add new resident" do 
+    assert_routing({ method: 'get', path: '/residents/new' }, { controller: "residents", action: "new" })
+  end
+
   test "should destroy resident" do 
     assert_routing({ method: 'delete', path: '/residents/1' }, { controller: "residents", action: "destroy", id: "1" })
+  end
+
+  test "should edit resident" do 
+    assert_routing({ method: 'get', path: '/residents/1/edit' }, { controller: "residents", action: "edit", id: "1" })
+  end
+
+  test "should update resident" do 
+    assert_routing({ method: 'put', path: '/residents/1' }, { controller: "residents", action: "update", id: "1" })
   end
 
 end
