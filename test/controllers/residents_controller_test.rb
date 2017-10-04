9605344c02c3dbd24d7136b4135b6f95c39be568
Chris Hunter <orkem2@gmail.com>
2017-10-04 14:35:29 +0100

    adds routing tests to residents controller

--- test/controllers/residents_controller_test.rb
+++ test/controllers/residents_controller_test.rb
@@ -2,9 +2,16 @@ require 'test_helper'
 
 class ResidentsControllerTest < ActionDispatch::IntegrationTest
   
-  # test "should get index" do
-  #   get root_url
-  #   assert_response :success
-  # end
+  test "should get index" do
+    assert_routing({ method: 'get', path: '/' }, { controller: "residents", action: "index" })
+  end
+
+  test "should get show" do
+    assert_routing({ method: 'get', path: '/residents/1' }, { controller: "residents", action: "show", id: "1" })
+  end
+
+  test "should destroy resident" do 
+    assert_routing({ method: 'delete', path: '/residents/1' }, { controller: "residents", action: "destroy", id: "1" })
+  end
 
 end
